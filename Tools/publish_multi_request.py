3da3f3488e0c2fa738d9cefeac62a0e58aa1c13c
MaxSMokeSkaarj <vnazarov@smokeofanarchy.ru>
2025-08-05 20:27:05 +1000

    Изменение названия ветки в CDN

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -16,7 +16,7 @@ RELEASE_DIR = "release"
 # Forks should change these to publish to their own infrastructure.
 #
 ROBUST_CDN_URL = "https://cdn.ss14.smokeofanarchy.ru/"
-FORK_ID = "main"
+FORK_ID = "sandbox"
 
 def main():
     parser = argparse.ArgumentParser()
